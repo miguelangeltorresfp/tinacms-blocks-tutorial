1893a2c7daad1bbb72fca917e10a0a5eacc24768
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-08-04 04:43:22 +0200

    tutorial-finished

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { Hero } from './components/Hero';
+// Import `useForm` and `usePlugin`
+import { useForm, usePlugin, useCMS } from 'tinacms';
+// Import `InlineForm`
+import { InlineForm, InlineBlocks } from 'react-tinacms-inline';
+import { heroBlock } from './components/Hero';
+import { imagesBlock } from './components/Images';
+import { paragraphBlock } from './components/Paragraph';
+import { featureListBlock } from './components/FeatureList';
 import data from './data/data.json';
 
 export default function Home() {
+  const cms = useCMS();
+
+  // Create a config object
+  const formConfig = {
+    id: './data/data.json',
+    initialValues: data,
+    onSubmit() {
+      cms.alerts.success('Saved!');
+    },
+  };
+
+  // Create and register the form.
+  const [pageData, form] = useForm(formConfig);
+  usePlugin(form);
+
+  // Use the return data now connected with a TinaCMS form
   return (
-    <div className='home'>
-      <Hero {...data} />
-    </div>
+    // Wrap `InlineForm` around `Hero`, pass the form
+    <InlineForm form={form}>
+      <InlineBlocks name="blocks" blocks={HOME_BLOCKS} />
+    </InlineForm>
   );
 }
+
+const HOME_BLOCKS = {
+  hero: heroBlock,
+  images: imagesBlock,
+  paragraph: paragraphBlock,
+  features: featureListBlock,
+};
---
+++ src/components/Hero copy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/hero.css';
+
+export function Hero({ headline, subtext }) {
+  return (
+    <div className="hero">
+      <div className="wrapper wrapper--narrow">
+        <h1>{headline}</h1>
+        <p>{subtext}</p>
+      </div>
+    </div>
+  );
+}
